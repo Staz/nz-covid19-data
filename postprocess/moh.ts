21c282f303e3de29dc8bbdf9316fd21eb1208dae
Ben Stasiewicz <ben@staz.net.nz>
2022-02-19 12:10:58 +1300

    Add: Other MOH stats pages

--- postprocess/moh.ts
+++ postprocess/moh.ts
@@ -3,22 +3,42 @@
   The above example will become "2022-02-17.html" 
 */
 import dayjs from 'https://cdn.skypack.dev/dayjs';
+import { dirname } from 'https://deno.land/std@0.126.0/path/mod.ts';
 
 const inputFile = Deno.args[0];
 const html = Deno.readTextFileSync(inputFile);
+let dateRegex: RegExp;
 
-const regex =
-  /All data on this page relates to cases recorded prior to (?<date>.+)\./;
-const match = regex.exec(html);
+if (inputFile.endsWith('covid-19-current-cases.html')) {
+  dateRegex =
+    /All data on this page relates to cases recorded prior to (?<date>.+)\./;
+} else if (inputFile.endsWith('covid-19-case-demographics.html')) {
+  dateRegex =
+    /All data on this page relates to cases recorded prior to (?<date>.+)\./;
+} else if (inputFile.endsWith('covid-19-source-cases.html')) {
+  dateRegex =
+    /All data on this page relates to cases recorded prior to (?<date>.+)\./;
+} else if (inputFile.endsWith('testing-covid-19.html')) {
+  dateRegex =
+    /All data on this page relates to tests processed prior to (?<date>.+)\./;
+} else if (inputFile.endsWith('covid-19-vaccine-data.html')) {
+  dateRegex = /Data in this section is as at (?<date>.+) and is updated daily/;
+} else {
+  throw 'Unrecognised file: ' + inputFile;
+}
+
+const match = dateRegex.exec(html);
 
 if (!match?.groups?.date) {
-  throw 'Unable to work out what date the data relates to';
+  throw 'Unable to work out what date the data relates to: ' + inputFile;
   // TODO: Save to a fail directory
 }
 
 const dateString = match.groups.date;
 const date = dayjs(dateString).format('YYYY-MM-DD');
 
-const outputFile = `./data/moh/covid-19-case-demographics/${date}.html`;
+const outputDir = dirname(inputFile);
+const outputFile = `./${outputDir}/${date}.html`;
+
 Deno.writeTextFileSync(outputFile, html);
 Deno.remove(inputFile);
