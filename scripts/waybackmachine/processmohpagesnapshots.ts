01252f727b7b5d1374c460e6e1134d3fd0e05ce4
Ben Stasiewicz <ben@staz.net.nz>
2022-02-23 07:45:51 +1300

    Add strict date parsing and formats to MOH page parsing

--- scripts/waybackmachine/processmohpagesnapshots.ts
+++ scripts/waybackmachine/processmohpagesnapshots.ts
@@ -6,15 +6,20 @@ const customParseFormat = require('dayjs/plugin/customParseFormat');
 dayjs.extend(customParseFormat);
 
 const POSSIBLE_DATE_BLURB_REGEXES = [
-  /All data on this page relates to cases recorded prior to (?<date>.+)\./,
-  /All information on this page is as at (?<date>.+) unless otherwise stated/,
-  /All data on this page is as at (?<date>.+) unless otherwise stated/,
-  /Data in this section is as at (?<date>.+) and/, // covid-19-vaccine-data
-  /All data on this page relates to tests processed prior to (?<date>.+)/, // testing-covid-19
+  /All data on this page relates to cases recorded prior to (?<date>.+?)\./,
+  /All information on this page is as at (?<date>.+?) unless otherwise stated/,
+  /All data on this page is as at (?<date>.+?) unless otherwise stated/,
+  /Data in this section is as at (?<date>.+?) and/, // covid-19-vaccine-data
+  /All data on this page relates to tests processed prior to (?<date>.+?)/, // testing-covid-19
 ];
 
-const POSSIBLE_DATE_FORMATS = ['h:mm a D MMMM YYYY', 'h:mma [on] D MMMM YYYY'];
-const OUTPUT_FILE_DATE_FORMAT = 'YYYY-MM-DD[T]HH:mm';
+const POSSIBLE_DATE_FORMATS = [
+  'h:mm a D MMMM YYYY',
+  'h:mma D MMMM YYYY',
+  'h:mma [on] D MMMM YYYY',
+];
+
+const OUTPUT_FILE_DATE_FORMAT = 'YYYY-MM-DD[T]HH[:]mm';
 
 const getDataDate = (input: string): string | null => {
   for (let regex of POSSIBLE_DATE_BLURB_REGEXES) {
@@ -22,7 +27,7 @@ const getDataDate = (input: string): string | null => {
 
     if (match && match.groups.date) {
       const dateString = match.groups.date;
-      const date = dayjs(dateString, POSSIBLE_DATE_FORMATS);
+      const date = dayjs(dateString, POSSIBLE_DATE_FORMATS, true);
 
       if (!date.isValid()) {
         throw new Error('Failed to parse date: ' + dateString);
