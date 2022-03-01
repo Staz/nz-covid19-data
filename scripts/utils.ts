648ab583bbe1252038e7845aba933b1f5c401a80
Ben Stasiewicz <ben@staz.net.nz>
2022-03-02 08:31:40 +1300

    Add util for reading JSON file

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -18,3 +18,8 @@ export const cleanHtml = (inputHtml: string) => {
   // Sigh, some MOH pages are riddled with these leading my regex to break
   return inputHtml.replace(/&nbsp;/g, ' ');
 };
+
+export const parseJsonFile = async (path: string) => {
+  const json = await Deno.readTextFile(path);
+  return JSON.parse(json);
+};
