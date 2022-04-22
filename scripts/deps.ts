ff109a85471b2478a3952685f4fa825fa513f92f
Ben Stasiewicz <ben@staz.net.nz>
2022-04-22 22:09:00 +1200

    Try different version of dayjs

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -7,4 +7,4 @@
 
 export { default as cheerio } from 'https://esm.sh/cheerio@1.0.0-rc.10';
 
-export { default as dayjs } from 'https://cdn.skypack.dev/dayjs@1.11.0';
+export { default as dayjs } from 'https://cdn.skypack.dev/dayjs@1.10.7?dts';
