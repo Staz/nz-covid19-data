5ef177cb56b73e83559fbe901151558682381df8
Ben Stasiewicz <ben@staz.net.nz>
2023-02-12 13:23:43 +1300

    Add debugging to statsnz

--- postprocess/statsnz.ts
+++ postprocess/statsnz.ts
@@ -3,4 +3,5 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
 const inputFileName = Deno.args[0];
 const json = await readJSON(inputFileName);
 
+console.log(`Writing ${json.value.length} entries to ${inputFileName}`)
 await writeJSON(inputFileName, json.value);
